2f30c6a3b856a9807e5185e761d125e2850e981f
Aram Kocharyan <akocharyan@fb.com>
2018-03-31 19:12:12 -0700

    Update coffeescript

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name: 'urbanetic:accounts-local',
   summary: 'Creates local users and an admin.',
   git: 'https://github.com/urbanetic/meteor-accounts-local.git',
-  version: '1.0.0'
+  version: '1.0.0_1'
 });
 
 Package.on_use(function(api) {
   api.versionsFrom('METEOR@1.6.1');
   api.use([
-    'coffeescript@1.2.3',
+    'coffeescript@2.2.1_1',
     'underscore',
     'accounts-password',
     'urbanetic:utility@2.0.0',
